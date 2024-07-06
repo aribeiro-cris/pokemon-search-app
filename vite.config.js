1ffc761bdf553df887ed43e5c483671536f4ace9
Andreia <andreiacribeiro.5@gmail.com>
2024-07-06 22:12:29 +0100

    add new vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/pokemon-search-app",
   plugins: [react()],
-  base: "/vite-deploy"
 })
