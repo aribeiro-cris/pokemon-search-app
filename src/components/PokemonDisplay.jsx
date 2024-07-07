c9112ab084572299b2999b42bbece96aaf79b90c
Andreia <andreiacribeiro.5@gmail.com>
2024-07-07 20:54:10 +0100

    add kgs and cms

--- src/components/PokemonDisplay.jsx
+++ src/components/PokemonDisplay.jsx
@@ -12,8 +12,8 @@ function PokemonDisplay({ sprite, pokemonName, id, weight, height, types, errorM
             {sprite && <img src={sprite} className="sprite-img" alt="sprite image" />}
             {pokemonName && <p>Pokemón: {capitalizeFirstLetter(pokemonName)}</p>}
             {id && <p>ID: {id}</p>}
-            {weight && <p>Weight: {weight}</p>}
-            {height && <p>Height: {height}</p>}
+            {weight && <p>Weight: {weight} kgs</p>}
+            {height && <p>Height: {height} cms</p>}
             {types.length > 0 && <p>Types: {capitalizeFirstLetter(types.join(', '))}</p>}
             {errorMessage && <div><p>{errorMessage}</p></div>}
         </div>
